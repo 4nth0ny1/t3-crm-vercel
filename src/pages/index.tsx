b8497fdbecabda91880ede1554c3350cc5467466
4nth0ny1 <acatullo4@gmail.com>
2023-10-07 17:28:53 -0500

    adjusted alignment of sign in page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,17 +16,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {!sessionData && (
-        <main className=" flex min-h-screen flex-row">
+        <main className="flex h-screen flex-row">
           <div className="w-1/2">
-            <img src="/city.png" alt="city.png" />
+            <img src="/city.png" alt="city.png" className="h-full w-full" />
           </div>
           <div className="flex w-1/2 flex-col items-center justify-center gap-12 px-4 py-16">
-            <h1 className="text-3xl">T3 CRM</h1>
-            <div className="flex flex-row gap-12 border-2 border-blue-400 p-20">
-              <button onClick={() => signIn("google")}>
+            <h1 className="text-5xl font-bold">T3 CRM</h1>
+            <h2 className="w-3/4 text-center text-2xl italic">
+              The simplest way to stay in touch with your customers and achieve
+              your goals.
+            </h2>
+            <p className="text-xl">
+              Please sign in with one of the two options ...
+            </p>
+            <div className="flex flex-row gap-12">
+              <button className="text-5xl" onClick={() => signIn("google")}>
                 <FcGoogle />
               </button>
-              <button onClick={() => signIn("github")}>
+              <button className="text-5xl" onClick={() => signIn("github")}>
                 <FiGithub />
               </button>
             </div>
