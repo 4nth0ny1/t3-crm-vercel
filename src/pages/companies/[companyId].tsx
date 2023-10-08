892128f5db9a1a82039ffbd2c88d3439f266b0b0
4nth0ny1 <acatullo4@gmail.com>
2023-10-08 17:45:01 -0500

    added new design for company page

--- src/pages/companies/[companyId].tsx
+++ src/pages/companies/[companyId].tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import Navbar from "~/components/Navbar";
 import { api } from "~/utils/api";
 
 export default function CompanyPage() {
@@ -7,6 +8,32 @@ export default function CompanyPage() {
   const companyId = router.query.companyId as string;
 
   const { data: company } = api.company.getOneCompany.useQuery({ companyId });
-  console.log(company);
-  return <div>{company?.name}</div>;
+
+  return (
+    <div>
+      <Navbar />
+      <div className="flex w-full flex-row gap-4 px-12 py-4">
+        <div className="flex w-2/3 flex-col">
+          <div className="mb-4 bg-gray-200 p-4">
+            <h2>{company?.name}</h2>
+            <p>{company?.phone}</p>
+            <p>
+              {company?.city}, {company?.state}
+            </p>
+          </div>
+          <div className="bg-gray-200 p-4">
+            <h2>Notes</h2>
+          </div>
+        </div>
+        <div className="flex w-1/3 flex-col">
+          <div className="mb-4 bg-gray-200 p-4">
+            <h2>Contacts</h2>
+          </div>
+          <div className="bg-gray-200 p-4">
+            <h2>Opportunities</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
