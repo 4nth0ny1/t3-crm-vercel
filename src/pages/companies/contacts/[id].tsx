2af5ae869a26802a4342a07ccbc5229f24b2e836
4nth0ny1 <acatullo4@gmail.com>
2023-10-18 22:07:39 -0500

    swapped the notes and attempts sections

--- src/pages/companies/contacts/[id].tsx
+++ src/pages/companies/contacts/[id].tsx
@@ -45,29 +45,6 @@ export default function ContactPage() {
               <p className="text-xl">{contact?.email}</p>
             </div>
           </div>
-          <div className="bg-base-200 p-4">
-            <div className="mb-4 flex flex-row justify-between">
-              <h2 className="text-xl">Notes</h2>
-            </div>
-            {/* {notes?.map((note) => {
-              return (
-                <div className="mb-8 flex flex-row justify-between border-b border-gray-500 pb-4">
-                  <p>{note?.content}</p>
-                  <div className="flex flex-col">
-                    <p>{`${dayjs(note?.createdAt).fromNow()}`}</p>
-                    <div className="flex flex-row justify-end">
-                      <AiFillDelete
-                        onClick={() => deleteMutation(note?.id)}
-                        className=" text-right text-2xl text-red-600 hover:cursor-pointer"
-                      />
-                    </div>
-                  </div>
-                </div>
-              );
-            })} */}
-          </div>
-        </div>
-        <div className="flex w-1/3 flex-col">
           <div className="mb-4 flex flex-col gap-4 bg-base-200 p-4">
             <div className="flex flex-col justify-between">
               <h2 className="text-xl">Attempts</h2>
@@ -94,6 +71,29 @@ export default function ContactPage() {
                 );
               })}
             </div>
+          </div>
+        </div>
+        <div className="flex w-1/3 flex-col">
+          <div className="bg-base-200 p-4">
+            <div className="mb-4 flex flex-row justify-between">
+              <h2 className="text-xl">Notes</h2>
+            </div>
+            {/* {notes?.map((note) => {
+              return (
+                <div className="mb-8 flex flex-row justify-between border-b border-gray-500 pb-4">
+                  <p>{note?.content}</p>
+                  <div className="flex flex-col">
+                    <p>{`${dayjs(note?.createdAt).fromNow()}`}</p>
+                    <div className="flex flex-row justify-end">
+                      <AiFillDelete
+                        onClick={() => deleteMutation(note?.id)}
+                        className=" text-right text-2xl text-red-600 hover:cursor-pointer"
+                      />
+                    </div>
+                  </div>
+                </div>
+              );
+            })} */}
           </div>
           <div className="bg-base-200 p-4">
             <h2>Opportunities</h2>
